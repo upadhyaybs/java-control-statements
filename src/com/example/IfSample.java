d563b2bc6078799704c84d5b68f700f281d4e72f
B Shankar Upadhyay <upadhyaybs@live.com>
2021-07-14 15:53:10 -0500

    initial commit

---
+++ src/com/example/IfSample.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class IfSample {
+
+	public static void main(String[] args) {
+		int x, y;
+
+        x = 10;
+        y = 20;
+        
+        if (x < y) {
+            System.out.println("x is less than y");
+        }
+
+       	x = x * 2;
+
+        if (x == y)
+            System.out.println("x now equal to y");
+
+       	x = x * 2;
+       
+        if (x > y)
+            System.out.println("x now greater than y");
+
+        if (x == y)
+            System.out.println("you won't see this");
+
+	}
+
+}
